3442cc0f0305c8dfd210d162661dac259d124c77
wade.hawk <junhyi.park@gmail.com>
2019-06-26 22:37:13 +0900

    challenge init

---
+++ bitcoin-flutter-challenge-start-master/lib/coin_data.dart
@@ -0,0 +1,38 @@
+//TODO: Add your imports here.
+
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoList = [
+  'BTC',
+  'ETH',
+  'LTC',
+];
+
+const bitcoinAverageURL =
+    'https://apiv2.bitcoinaverage.com/indices/global/ticker';
+
+class CoinData {
+  //TODO: Create your getCoinData() method here.
+}
